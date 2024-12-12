2320a2b5b6a2e23287cc19ea9ff99a68500848ff
Kumbhani-Juli <julikumbhani5981@gmail.com>
2024-12-12 13:41:35 +0530

    12-12-24

--- src/components/Stories.js
+++ src/components/Stories.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import "../assets/styles/stories.css";
 import storiesData from "../Data/StoriesData";
 const Stories = () => {
@@ -28,29 +28,52 @@ const Stories = () => {
 			? title.substring(0, maxLength) + "..."
 			: title;
 	};
+	const [Title, setTitle] = useState(storiesTitle[0]);
+	const selectedStoriesData =
+		Title.title === "All Stories"
+			? storiesData
+			: storiesData.filter(
+					(data) => data.storyType.toLowerCase() === Title.title.toLowerCase()
+			  );
 	return (
 		<>
 			<div className="stories-container">
 				<div className="stories-title">
 					{storiesTitle?.map((title, index) => (
-						<p key={index}>{title?.title}</p>
+						<p
+							key={index}
+							onClick={() => {
+								setTitle(title);
+							}}
+						>
+							{title?.title}
+						</p>
 					))}
 				</div>
 				<div className="row mx-0 stories-body">
-					{storiesData?.map((storiesData, index) => (
-						<div className="col-md-4 stories-body-content" key={index}>
-							<div className="stories-body-content-box">
-								<div className="stories-body-box-img">
-									<img src={storiesData?.bgImg} className="img-fluid" />
-									<button>{storiesData?.storyType}</button>
+					{selectedStoriesData.length > 0 ? (
+						<>
+							{" "}
+							{selectedStoriesData?.map((storiesData, index) => (
+								<div className="col-md-4 stories-body-content" key={index}>
+									<div className="stories-body-content-box">
+										<div className="stories-body-box-img">
+											<img src={storiesData?.bgImg} className="img-fluid" />
+											<button>{storiesData?.storyType}</button>
+										</div>
+										<div className="stories-inner-content">
+											<p>{truncateTitle(storiesData?.title, 30)}</p>
+											<h1>{truncateTitle(storiesData?.content, 50)}</h1>
+										</div>
+									</div>
 								</div>
-								<div className="stories-inner-content">
-									<p>{truncateTitle(storiesData?.title, 30)}</p>
-									<h1>{truncateTitle(storiesData?.content, 50)}</h1>
-								</div>
-							</div>
-						</div>
-					))}
+							))}
+						</>
+					) : (
+						<>
+							<h1 style={{ textAlign: "center" }}>No Data Available.</h1>
+						</>
+					)}
 				</div>
 			</div>
 		</>
